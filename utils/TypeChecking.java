b76e94156468c7b411ca69f0bc0cb789f9187c40
Zbyněk <Fier>
2013-05-15 23:09:50 +0200

    wow :-D

--- utils/TypeChecking.java
+++ utils/TypeChecking.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package utils;
 
 import utils.IRVisitor;
@@ -18,145 +17,201 @@ import ir.UnaryExpression;
 import ir.Variable;
 import ir.WhileStatement;
 
+public class TypeChecking implements IRVisitor {
 
-public class TypeChecking implements IRVisitor{
-   
     @Override
     public void visit(AssigmentStatement st) {
         String varType, expressionType;
         varType = st.getVariable().getType();
         expressionType = st.getExpression().getType();
-        
-        if(!varType.matches("ERROR") && !expressionType.matches("ERROR"))
-        {
-            if((varType.matches("FLOAT") || varType.matches("INT")) && (expressionType.matches("FLOAT")|| expressionType.matches("INT")))
-            {}
-            else if(!varType.matches(expressionType)){
-                    MyError.addError(new MyError( st.getLine(),st.getColumn(), "Bad assingment of variable: "+ st.getVariable().getName() + ".\n Expected: " + varType +"\n Found: " + expressionType +"\n"));
-                }
+
+        if (!varType.matches("ERROR") && !expressionType.matches("ERROR")) {
+            if ((varType.matches("FLOAT") || varType.matches("INT")) && (expressionType.matches("FLOAT") || expressionType.matches("INT"))) {
+            } else if (!varType.matches(expressionType)) {
+                MyError.addError(new MyError(st.getLine(), st.getColumn(), "Bad assingment of variable: " + st.getVariable().getName() + ".\n Expected: " + varType + "\n Found: " + expressionType + "\n"));
+            }
         }
     }
-   
+
     @Override
     public void visit(BinaryExpression exp) {
-        final String left = exp.getLeft().getType(), right= exp.getRight().getType();
-        if(!left.matches("ERROR") && !right.matches("ERROR"))
-        {
-            if(left.matches(right) && left.matches("BOOLEAN"))
-            {
-                switch(exp.getOperator())
-                {
-                    case "||"   :exp.setType("BOOLEAN"); break;
-                    case "&&"   :exp.setType("BOOLEAN"); break;
-                    case "=="   :exp.setType("BOOLEAN"); break;
-                    case "!="   :exp.setType("BOOLEAN"); break;
-                    case ">"    :exp.setType("BOOLEAN"); break;
-                    case "<"    :exp.setType("BOOLEAN"); break;
-                    default     :exp.setType("ERROR");   break;       
+        final String left = exp.getLeft().getType(), right = exp.getRight().getType();
+        if (!left.matches("ERROR") && !right.matches("ERROR")) {
+            if (left.matches(right) && left.matches("BOOLEAN")) {
+                switch (exp.getOperator()) {
+                    case "||":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "&&":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "==":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "!=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<":
+                        exp.setType("BOOLEAN");
+                        break;
+                    default:
+                        exp.setType("ERROR");
+                        break;
                 }
-            }
-            else if(left.matches(right) && left.matches("STRING"))
-            {
-               switch(exp.getOperator())
-                {
-                    case "."    :exp.setType("STRING"); break;
-                    case "=="   :exp.setType("BOOLEAN"); break;
-                    case "!="   :exp.setType("BOOLEAN"); break;
-                    case ">"    :exp.setType("BOOLEAN"); break;
-                    case "<"    :exp.setType("BOOLEAN"); break;
-                    case "<="   :exp.setType("BOOLEAN"); break;
-                    case ">="   :exp.setType("BOOLEAN"); break;
-                    default     :exp.setType("ERROR");   break;       
-                } 
-            }
-            else if(left.matches(right) && left.matches("INT"))
-            {
-                switch(exp.getOperator())
-                {
-                    case "%"    :exp.setType("INT"); break;
-                    case "=="   :exp.setType("BOOLEAN"); break;
-                    case "!="   :exp.setType("BOOLEAN"); break;
-                    case ">"    :exp.setType("BOOLEAN"); break;
-                    case "<"    :exp.setType("BOOLEAN"); break;
-                    case "<="   :exp.setType("BOOLEAN"); break;
-                    case ">="   :exp.setType("BOOLEAN"); break;
-                    case "+"    :exp.setType("INT"); break;
-                    case "-"    :exp.setType("INT"); break;
-                    case "/"    :exp.setType("INT"); break;
-                    case "*"    :exp.setType("INT"); break;
-                    default     :exp.setType("ERROR");   break;       
-                } 
-            }
-            else if((left.matches("FLOAT") || left.matches("INT")) && (right.matches("FLOAT")|| right.matches("INT")))
-            {
-                switch(exp.getOperator())
-                {
-                    case "=="   :exp.setType("BOOLEAN"); break;
-                    case "!="   :exp.setType("BOOLEAN"); break;
-                    case ">"    :exp.setType("BOOLEAN"); break;
-                    case "<"    :exp.setType("BOOLEAN"); break;
-                    case "<="   :exp.setType("BOOLEAN"); break;
-                    case ">="   :exp.setType("BOOLEAN"); break;
-                    case "+"    :exp.setType("FLOAT"); break;
-                    case "-"    :exp.setType("FLOAT"); break;
-                    case "/"    :exp.setType("FLOAT"); break;
-                    case "*"    :exp.setType("FLOAT"); break;
-                    default     :exp.setType("ERROR");   break;       
-                } 
-            }
-            else
-            {
+            } else if (left.matches(right) && left.matches("STRING")) {
+                switch (exp.getOperator()) {
+                    case ".":
+                        exp.setType("STRING");
+                        break;
+                    case "==":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "!=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    default:
+                        exp.setType("ERROR");
+                        break;
+                }
+            } else if (left.matches(right) && left.matches("INT")) {
+                switch (exp.getOperator()) {
+                    case "%":
+                        exp.setType("INT");
+                        break;
+                    case "==":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "!=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "+":
+                        exp.setType("INT");
+                        break;
+                    case "-":
+                        exp.setType("INT");
+                        break;
+                    case "/":
+                        exp.setType("INT");
+                        break;
+                    case "*":
+                        exp.setType("INT");
+                        break;
+                    default:
+                        exp.setType("ERROR");
+                        break;
+                }
+            } else if ((left.matches("FLOAT") || left.matches("INT")) && (right.matches("FLOAT") || right.matches("INT"))) {
+                switch (exp.getOperator()) {
+                    case "==":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "!=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "<=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case ">=":
+                        exp.setType("BOOLEAN");
+                        break;
+                    case "+":
+                        exp.setType("FLOAT");
+                        break;
+                    case "-":
+                        exp.setType("FLOAT");
+                        break;
+                    case "/":
+                        exp.setType("FLOAT");
+                        break;
+                    case "*":
+                        exp.setType("FLOAT");
+                        break;
+                    default:
+                        exp.setType("ERROR");
+                        break;
+                }
+            } else {
                 exp.setType("ERROR");
-                MyError.addError(new MyError( exp.getLine(),exp.getColumn(), "You cant count: "+ left + " " + exp.getOperator() +" " + right));
+                MyError.addError(new MyError(exp.getLine(), exp.getColumn(), "You cant count: " + left + " " + exp.getOperator() + " " + right));
             }
-                
-        }
-        else
+
+        } else {
             exp.setType("ERROR");
+        }
     }
-   
+
     @Override
     public void visit(BlockStatement st) {
         // Nothing to do
     }
-   
+
     @Override
     public void visit(ConditionalExpression exp) {
         String cond = exp.getCondition().getType();
         String left = exp.getLeft().getType();
         String right = exp.getRight().getType();
-        
-        if(!cond.matches("ERROR") && !cond.matches("ERROR") && !cond.matches("ERROR"))
-        {
-            if(cond.matches("BOOLEAN"))
-            {
-                if(left.matches(right))
+
+        if (!cond.matches("ERROR") && !cond.matches("ERROR") && !cond.matches("ERROR")) {
+            if (cond.matches("BOOLEAN")) {
+                if (left.matches(right)) {
                     exp.setType(left);
-                else if((left.matches("FLOAT") || left.matches("INT")) && (right.matches("FLOAT")|| right.matches("INT")))
+                } else if ((left.matches("FLOAT") || left.matches("INT")) && (right.matches("FLOAT") || right.matches("INT"))) {
                     exp.setType("FLOAT");
-                else
-                {
+                } else {
                     exp.setType("ERROR");
-                    MyError.addError(new MyError( exp.getLine(),exp.getColumn(), "Different type in results of ? operator.\n Expected: "+ left +"\n Found: "+ right));
+                    MyError.addError(new MyError(exp.getLine(), exp.getColumn(), "Different type in results of ? operator.\n Expected: " + left + "\n Found: " + right));
                 }
-            }
-            else
-            {
+            } else {
                 exp.setType("ERROR");
-                MyError.addError(new MyError( exp.getLine(),exp.getColumn(), "Bad ? condition expression.\n Expected: BOOLEAN\n Found: "+ cond));
+                MyError.addError(new MyError(exp.getLine(), exp.getColumn(), "Bad ? condition expression.\n Expected: BOOLEAN\n Found: " + cond));
             }
-        }
-        else
+        } else {
             exp.setType("ERROR");
+        }
     }
-   
+
     @Override
     public void visit(IfStatement st) {
         String cond = st.getCondition().getType();
-        if(!cond.matches("ERROR"))
-            if(cond.matches("BOOLEAN")){}
-            else
-                MyError.addError(new MyError( st.getLine(),st.getColumn(), "Bad IF condition expression.\n Expected: BOOLEAN\n Found: "+ cond)); 
+        if (!cond.matches("ERROR")) {
+            if (cond.matches("BOOLEAN")) {
+            } else {
+                MyError.addError(new MyError(st.getLine(), st.getColumn(), "Bad IF condition expression.\n Expected: BOOLEAN\n Found: " + cond));
+            }
+        }
     }
 
     @Override
@@ -178,65 +233,76 @@ public class TypeChecking implements IRVisitor{
     @Override
     public void visit(UnaryExpression exp) {
         String type = exp.getExpression().getType();
-        if(!type.matches("ERROR"))
-        {
-            if((type.matches("INT") || type.matches("FLOAT")) && exp.getOperator().matches("-"))
+        if (!type.matches("ERROR")) {
+            if ((type.matches("INT") || type.matches("FLOAT")) && exp.getOperator().matches("-")) {
                 exp.setType(type);
-            else if(type.matches("BOOLEAN")&& exp.getOperator().matches("!"))
+            } else if (type.matches("BOOLEAN") && exp.getOperator().matches("!")) {
                 exp.setType(type);
-            else
-            {
+            } else {
                 exp.setType("ERROR");
-                MyError.addError(new MyError( exp.getLine(),exp.getColumn(), "You cant do: "+ exp.getOperator() + " " + type));
+                MyError.addError(new MyError(exp.getLine(), exp.getColumn(), "You cant do: " + exp.getOperator() + " " + type));
             }
-        }
-        else
+        } else {
             exp.setType("ERROR");
+        }
     }
 
     @Override
     public void visit(Variable exp) {
-        
     }
 
     @Override
     public void visit(WhileStatement st) {
         String cond = st.getCondition().getType();
-        if(!cond.matches("ERROR"))
-            if(cond.matches("BOOLEAN")){}
-            else
-                MyError.addError(new MyError( st.getLine(),st.getColumn(), "Bad WHILE condition expression.\n Expected: BOOLEAN\n Found: "+ cond));
-    }
-
-    private String FindType(Object obj)
-    {
-        switch(obj.getClass().getName())
-        {
-            case "java.lang.Integer" : return "INT";
-            case "java.lang.Double" : return "DOUBLE";
-            case "java.lang.Boolean" : return "BOOLEAN";
-            case "java.lang.String" : return "STRING";
-            default : return "ERROR";
+        if (!cond.matches("ERROR")) {
+            if (cond.matches("BOOLEAN")) {
+            } else {
+                MyError.addError(new MyError(st.getLine(), st.getColumn(), "Bad WHILE condition expression.\n Expected: BOOLEAN\n Found: " + cond));
+            }
         }
     }
-    
-    public static String FindOperator(String operator)
-    {
-        switch(operator)
-        {
-            case "=="   : return "eq";
-            case "!="   : return "eq\nnot";
-            case ">"    : return "gt";
-            case "<"    : return "lt";
+
+    private String FindType(Object obj) {
+        switch (obj.getClass().getName()) {
+            case "java.lang.Integer":
+                return "INT";
+            case "java.lang.Double":
+                return "DOUBLE";
+            case "java.lang.Boolean":
+                return "BOOLEAN";
+            case "java.lang.String":
+                return "STRING";
+            default:
+                return "ERROR";
+        }
+    }
+
+    public static String FindOperator(String operator) {
+        switch (operator) {
+            case "==":
+                return "eq";
+            case "!=":
+                return "eq\nnot";
+            case ">":
+                return "gt";
+            case "<":
+                return "lt";
             //case "<="   :exp.setType("BOOLEAN"); break;
             //case ">="   :exp.setType("BOOLEAN"); break;
-            case "+"    : return "add";
-            case "-"    : return "sub";
-            case "/"    : return "div";
-            case "*"    : return "mul";
-            case "%"    : return "mod";
-            case "."    : return "concat";
-            default     : return "error";
+            case "+":
+                return "add";
+            case "-":
+                return "sub";
+            case "/":
+                return "div";
+            case "*":
+                return "mul";
+            case "%":
+                return "mod";
+            case ".":
+                return "concat";
+            default:
+                return "error";
         }
     }
 }
