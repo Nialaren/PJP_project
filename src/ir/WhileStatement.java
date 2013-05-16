0c7e3d0d3ac95645b3f4a403be4026b50a935b85
Michal Šimon <michal.simon@centrum.cz>
2013-05-16 06:48:58 +0300

    Update WhileStatement.java

--- src/ir/WhileStatement.java
+++ src/ir/WhileStatement.java
@@ -2,33 +2,38 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ir;
 
 import utils.IRVisitor;
+import utils.JumpCounter;
 
 /**
  *
  * @author beh01
  */
-public class WhileStatement extends Statement{
+public class WhileStatement extends Statement {
 
     private BlockStatement statement;
     private Expression condition;
     private int line;
     private int column;
+    private int tLabel;
+    private int fLabel;
 
     public WhileStatement(BlockStatement statement, Expression condition, int line, int column) {
         this.statement = statement;
         this.condition = condition;
         this.line = line;
         this.column = column;
+        this.tLabel = JumpCounter.get();
+        this.fLabel = JumpCounter.get();
     }
 
-    public int getLine(){
+    public int getLine() {
         return line;
     }
-    public int getColumn(){
+
+    public int getColumn() {
         return column;
     }
 
@@ -49,11 +54,25 @@ public class WhileStatement extends Statement{
 
     @Override
     public String toString() {
-        return "while ("+condition.toString()+") do "+statement.toString()+" end;";
+        return "while (" + condition.toString() + ") do " + statement.toString() + " end;";
     }
 
     @Override
     public String getCode() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        StringBuilder output = new StringBuilder();
+        
+        output.append("#cycle begin\n");
+        output.append("LABEL " + this.tLabel + "\n");
+        
+        output.append(condition.getCode());
+        //output.append("LOAD "+condition.getId()+"\n");
+        output.append("FJMP " + this.fLabel + "\n");
+        output.append(statement.getCode());
+
+        output.append("JMP " + this.tLabel + "\n");
+        output.append("LABEL " + this.fLabel + "\n");
+        output.append("#cycle end\n");
+
+        return output.toString();
     }
 }
